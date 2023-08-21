84150e2250639bb15999b0220e5f607f9347ce41
JimChristian21 <jimchristianedullantes9@gmail.com>
2023-08-21 17:32:47 +0800

    Moved detect function to parent component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,52 @@ function App() {
 
   const [imageUrl, setImageUrl] = useState('');
   const [showFaceRecognition, setShowFaceRecognition] = useState(false);
+  const [box, setBox] = useState([]);
+
+  const calculateFaceLocation = (data) => {
+
+    console.log(data);
+  }
+
+  const handleDetect = () => {
+    const PAT = 'ea86c3ad3aaf469eae8ee0ba8f74c9cf';
+    const USER_ID = 'jim_christian';       
+    const APP_ID = 'Teset';
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": imageUrl
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(response => response.text())
+        .then(result => calculateFaceLocation(result))
+        .catch(error => console.log('error', error));
+
+    setShowFaceRecognition(true);
+  }
 
   return (
     <div className="App">
@@ -18,8 +64,12 @@ function App() {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm imageUrl={imageUrl} setImageUrl={setImageUrl} setShowFaceRecognition={setShowFaceRecognition}/>
-      { showFaceRecognition 
+      <ImageLinkForm 
+        imageUrl={imageUrl}
+        setImageUrl={setImageUrl}
+        handleDetect={handleDetect}
+      />
+      { showFaceRecognition
           && <FaceRecognition imageUrl={imageUrl}/>
       }
     </div>
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,48 +1,11 @@
 import React from "react";
 import './imageLinkForm.css';
 
-const ImageLinkForm = ({ imageUrl, setImageUrl, setShowFaceRecognition}) => {
- 
-    const handleSubmit = () => {
-
-        const PAT = 'ea86c3ad3aaf469eae8ee0ba8f74c9cf';
-        const USER_ID = 'jim_christian';       
-        const APP_ID = 'Teset';
-        const MODEL_ID = 'face-detection';
-        const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
-
-        const raw = JSON.stringify({
-            "user_app_id": {
-                "user_id": USER_ID,
-                "app_id": APP_ID
-            },
-            "inputs": [
-                {
-                    "data": {
-                        "image": {
-                            "url": imageUrl
-                        }
-                    }
-                }
-            ]
-        });
-
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': 'Key ' + PAT
-            },
-            body: raw
-        };
-
-        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-
-        setShowFaceRecognition(true);
-    }
+const ImageLinkForm = ({ 
+    imageUrl, 
+    setImageUrl,
+    handleDetect
+}) => {
 
     return (
         <div>
@@ -59,7 +22,7 @@ const ImageLinkForm = ({ imageUrl, setImageUrl, setShowFaceRecognition}) => {
                     />
                     <button 
                         className="w-30 grow f4 link ph3 pv2 div white bg-light-purple"
-                        onClick={handleSubmit}
+                        onClick={handleDetect}
                     >
                         Detect
                     </button>
