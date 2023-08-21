375679a8c7c1d37d2cf86eb32e20ea6ef92522f9
JimChristian21 <jimchristianedullantes9@gmail.com>
2023-08-21 16:46:55 +0800

    Added states and used it as a props to the components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
 import Rank from "./components/rank/Rank";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/faceRecognition/FaceRecognition";
 import ParticlesBg from "particles-bg";
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [imageUrl, setImageUrl] = useState('');
+  const [showFaceRecognition, setShowFaceRecognition] = useState(false);
+
   return (
     <div className="App">
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+      <ImageLinkForm imageUrl={imageUrl} setImageUrl={setImageUrl} setShowFaceRecognition={setShowFaceRecognition}/>
+      { showFaceRecognition 
+          && <FaceRecognition imageUrl={imageUrl}/>
+      }
     </div>
   ); 
 }
